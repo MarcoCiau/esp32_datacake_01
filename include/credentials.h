22c7d5498e7b4c8e30fae03e03c9259e25f5726e
Marco Ciau <marcoantoniociau@hotmail.com>
2026-02-06 10:38:56 -0600

    Add credentials.h for centralized WiFi and Datacake configuration
    Extract all credentials (WiFi SSID/password, Datacake URL/device ID)
    into a single header file for easier configuration and maintenance.

---
+++ include/credentials.h
@@ -0,0 +1,12 @@
+#ifndef CREDENTIALS_H
+#define CREDENTIALS_H
+
+// WiFi Credentials
+#define WIFI_SSID "XXXXXXXX"           // TODO: Update with your WiFi SSID
+#define WIFI_PASSWORD "XXXXXXXX"           // TODO: Update with your WiFi password
+
+// Datacake Configuration
+#define DATACAKE_SERVER_URL "https://api.datacake.co/integrations/api/XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX/"  // TODO: Update with your Datacake API URL
+#define DATACAKE_DEVICE_ID "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX"  // TODO: Update with your Datacake Device ID
+
+#endif // CREDENTIALS_H
